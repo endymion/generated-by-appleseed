ad1a8a9bcf189d6907c2b93a22eb0441ef4a23bc
Ryan Alyn Porter <rap@endymion.com>
2010-08-31 09:48:05 -0400

    Initial commit to generated-by-appleseed.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeneratedByAppleseed::Application.config.secret_token = 'fba52c28361a3a2973c3c83e84313090cde44843b0ef83340f660b5a204431975ef806b8d442aaff51f50cbfc914bfb97ca2a5c4a02a563c3d79c9346ae5e006'
